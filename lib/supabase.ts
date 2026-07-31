e7146e0f6c76936d02dc9bd2a37d1f788b7266f2
Mhikara <meydihikara@gmail.com>
2026-08-01 04:43:49 +0800

    Fix supabase.ts to use env variable names

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.https://wahfvbmptambkgasbcia.supabase.co;
-const supabaseKey = process.env.sb_publishable_n7GwD-Vv_JQqPh9us82p5A_YOHTJ_2C;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
